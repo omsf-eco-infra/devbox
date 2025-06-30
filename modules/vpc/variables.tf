73e40e12673dae46a78187c4ce8894dd8c717bf4
David W.H. Swenson <david.swenson@omsf.io>
2025-06-30 17:38:27 -0500

    Remove VPC subnet CIDRs as input variable
    Now we'll just take the right number of /24 subnets; one for each AZ in
    the region. Requires that user provides a /20 CIDR range or larger for
    our VPC.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -6,10 +6,6 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
-variable "subnet_cidr" {
-  description = "List of CIDR blocks for the subnets"
-  type        = list(string)
-}
 variable "ssh_cidr_blocks" {
   description = "List of CIDR blocks allowed SSH"
   type        = list(string)
