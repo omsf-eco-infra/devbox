4b1e79a7341480e86a1e2e50f7f7af42a4f4fd84
David W.H. Swenson <david.swenson@omsf.io>
2025-11-26 11:57:20 -0600

    Start DNS terraform

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -1,3 +1,8 @@
+locals {
+  dns_enabled    = var.dns_provider != "none"
+  use_cloudflare = var.dns_provider == "cloudflare"
+}
+
 resource "aws_ssm_parameter" "launch_template" {
   count       = length(var.launch_template_ids)
   name        = "${var.param_prefix}/launchTemplateId_az${count.index}"
@@ -21,4 +26,60 @@ resource "aws_ssm_parameter" "snapshot_table" {
   type        = "String"
   value       = var.snapshot_table_name
   overwrite   = true
+}
+
+resource "aws_ssm_parameter" "dns_provider" {
+  name        = "${var.param_prefix}/dns/provider"
+  description = "DNS provider configured for devbox instances"
+  type        = "String"
+  value       = var.dns_provider
+  overwrite   = true
+}
+
+resource "aws_ssm_parameter" "dns_zone" {
+  count       = local.dns_enabled ? 1 : 0
+  name        = "${var.param_prefix}/dns/zone"
+  description = "DNS zone used for devbox instance CNAME records"
+  type        = "String"
+  value       = var.dns_zone
+  overwrite   = true
+
+  lifecycle {
+    precondition {
+      condition     = length(trimspace(var.dns_zone)) > 0
+      error_message = "dns_zone must be set when dns_provider is not \"none\"."
+    }
+  }
+}
+
+resource "aws_ssm_parameter" "cloudflare_api_token" {
+  count       = local.use_cloudflare ? 1 : 0
+  name        = "${var.param_prefix}/secrets/cloudflare/apiToken"
+  description = "Cloudflare API token for devbox DNS management"
+  type        = "SecureString"
+  value       = var.cloudflare_api_token
+  overwrite   = true
+
+  lifecycle {
+    precondition {
+      condition     = length(trimspace(var.cloudflare_api_token)) > 0
+      error_message = "cloudflare_api_token must be set when dns_provider is \"cloudflare\"."
+    }
+  }
+}
+
+resource "aws_ssm_parameter" "cloudflare_zone_id" {
+  count       = local.use_cloudflare ? 1 : 0
+  name        = "${var.param_prefix}/secrets/cloudflare/zoneId"
+  description = "Cloudflare zone ID for devbox DNS management"
+  type        = "SecureString"
+  value       = var.cloudflare_zone_id
+  overwrite   = true
+
+  lifecycle {
+    precondition {
+      condition     = length(trimspace(var.cloudflare_zone_id)) > 0
+      error_message = "cloudflare_zone_id must be set when dns_provider is \"cloudflare\"."
+    }
+  }
 }
